6416a65076f9f90d667a91ee1f410add66f7d7a5
Chevalmuscle <marcantoinejean1997@gmail.com>
2018-12-09 01:03:26 -0500

    part 2

--- day09/day09.go
+++ day09/day09.go
@@ -19,49 +19,40 @@ func marbleScore(input string) int {
 
 	var list = list.New()
 	list.PushBack(0)
-	var current = list.Front()
+	var currentMarble = list.Front()
 	var currentPlayer = 1
 
-	//var winningPlayer = -1
 	var winningScore = -1
 
-	for marble := 1; marble <= lastMarble; marble++ {
+	for marble := 1; marble <= lastMarble*100; marble++ { // remove *100 for part 1
 		currentPlayer++
 		if currentPlayer > playerCount {
 			currentPlayer = 1
 		}
-
 		if marble%23 != 0 {
-			var e = current.Next()
+			var e = currentMarble.Next()
 			if e == nil {
 				e = list.Front()
 			}
-			current = list.InsertAfter(marble, e)
+			currentMarble = list.InsertAfter(marble, e)
 		} else {
 			for i := 0; i < 7; i++ {
-				current = current.Prev()
-				if current == nil {
-					current = list.Back()
+				currentMarble = currentMarble.Prev()
+				if currentMarble == nil {
+					currentMarble = list.Back()
 				}
 			}
-			scores[currentPlayer] += current.Value.(int)
+			scores[currentPlayer] += currentMarble.Value.(int)
 			scores[currentPlayer] += marble
 
 			if scores[currentPlayer] > winningScore {
 				winningScore = scores[currentPlayer]
-				//winningPlayer = currentPlayer
 			}
-			var toRemove = current
-			current = toRemove.Next()
+			var toRemove = currentMarble
+			currentMarble = toRemove.Next()
 			list.Remove(toRemove)
-
 		}
 	}
-	/*
-		for e := list.Front(); e != nil; e = e.Next() {
-			fmt.Print(e.Value, " ")
-		}*/
 
-	//fmt.Println("\nwinning elf", winningPlayer)
 	return winningScore
 }
--- day09/day09_test.go
+++ day09/day09_test.go
@@ -9,26 +9,23 @@ import (
 
 func TestMarbleScore(t *testing.T) {
 
+	// these are all the answers for part 1
 	input1 := utils.ReadLines("testInput.txt")[0]
 	testMarbleScore(t, input1, 32)
-
-	input2 := utils.ReadLines("input.txt")[0]
-	testMarbleScore(t, input2, 373597)
-
 	input3 := "10 players; last marble is worth 1618 points"
-	testMarbleScore(t, input3, 8317)
-
+	testMarbleScore(t, input3, 831)
 	input4 := "13 players; last marble is worth 7999 points"
 	testMarbleScore(t, input4, 146373)
-
 	input5 := "17 players; last marble is worth 1104 points"
 	testMarbleScore(t, input5, 2764)
-
 	input6 := "21 players; last marble is worth 6111 points"
 	testMarbleScore(t, input6, 54718)
-
 	input7 := "30 players; last marble is worth 5807 points"
 	testMarbleScore(t, input7, 37305)
+
+	// for part 2
+	finalInput := utils.ReadLines("input.txt")[0]
+	testMarbleScore(t, finalInput, 2954067253) // for part 1: 373597
 
 }
 
@@ -37,6 +34,6 @@ func testMarbleScore(t *testing.T, input string, expected int) {
 	output := marbleScore(input)
 
 	if output != expected {
-		t.Errorf("marbleScore part 1 was incorrect, got: %d, want: %d.", output, expected)
+		t.Errorf("marbleScore was incorrect, got: %d, want: %d.", output, expected)
 	}
 }
