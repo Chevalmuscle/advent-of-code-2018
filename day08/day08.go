6477a43eccd8253d544f79f13dfc51bc2661374d
Chevalmuscle <marcantoinejean1997@gmail.com>
2018-12-08 19:21:15 -0500

    part 2

--- day08/day08.go
+++ day08/day08.go
@@ -8,10 +8,8 @@ import (
 	"../utils"
 )
 
-func sumMetadata(input string) int {
+func sumMetadata(input string) (int, int) {
 	defer utils.TimeTaken(time.Now())
-
-	var sum = 0
 
 	var numbersString = strings.Split(input, " ")
 	var numbers = make([]int, len(numbersString))
@@ -19,19 +17,54 @@ func sumMetadata(input string) int {
 		numbers[i], _ = strconv.Atoi(number)
 	}
 
-	sum, _ = recursive(numbers)
-	return sum
+	sumPart1, _ := recursivePart1(numbers)
+	sumPart2, _ := recursivePart2(numbers)
+
+	return sumPart1, sumPart2
 }
 
-func recursive(numbers []int) (int, int) {
+func recursivePart2(numbers []int) (int, int) {
 	var numberOfChild = numbers[0]
 	var numberOfMeta = numbers[1]
 
-	var rest = numbers[2:]
-	var sum = 0
+	var valuesOfChild = make(map[int]int)
 	var index = 2
+	var rest = numbers[index:]
+	var sum = 0
+
 	for i := 0; i < numberOfChild; i++ {
-		sumTmp, indexTmp := recursive(rest)
+		sumTmp, indexTmp := recursivePart2(rest)
+		valuesOfChild[i+1] = sumTmp
+		//sum += sumTmp
+		index += indexTmp
+		rest = numbers[index:]
+	}
+
+	var i = index
+	for i < index+numberOfMeta {
+		var metadata = numbers[i]
+		//if _, isChild := valuesOfChild[metadata]; isChild {
+		if numberOfChild == 0 {
+			sum += metadata
+		} else {
+			sum += valuesOfChild[metadata]
+
+		}
+		//}
+		i++
+	}
+
+	return sum, i
+}
+
+func recursivePart1(numbers []int) (int, int) {
+	var numberOfChild = numbers[0]
+	var numberOfMeta = numbers[1]
+	var index = 2
+	var rest = numbers[index:]
+	var sum = 0
+	for i := 0; i < numberOfChild; i++ {
+		sumTmp, indexTmp := recursivePart1(rest)
 		sum += sumTmp
 		index += indexTmp
 		rest = numbers[index:]
--- day08/day08_test.go
+++ day08/day08_test.go
@@ -9,18 +9,21 @@ import (
 func TestSumMetadata(t *testing.T) {
 
 	input1 := utils.ReadLines("testInput.txt")[0]
-	testSumMetadata(t, input1, 138)
+	testSumMetadata(t, input1, 138, 66)
 
 	input2 := utils.ReadLines("input.txt")[0]
-	testSumMetadata(t, input2, 35911)
+	testSumMetadata(t, input2, 35911, 17206)
 
 }
 
-func testSumMetadata(t *testing.T, input string, expected int) {
+func testSumMetadata(t *testing.T, input string, expected1 int, expected2 int) {
 
-	output := sumMetadata(input)
+	output1, output2 := sumMetadata(input)
 
-	if output != expected {
-		t.Errorf("sumMetadata part 1 was incorrect, got: %d, want: %d.", output, expected)
+	if output1 != expected1 {
+		t.Errorf("sumMetadata part 1 was incorrect, got: %d, want: %d.", output1, expected1)
+	}
+	if output2 != expected2 {
+		t.Errorf("sumMetadata part 2 was incorrect, got: %d, want: %d.", output2, expected2)
 	}
 }
